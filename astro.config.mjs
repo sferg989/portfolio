affceab5a5da7a3ad074cfce94aea9ca23ba763b
Stephen Ferguson <sferg989@gmail.com>
2025-06-13 23:33:00 -0500

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,23 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     integrations: [
         svelte(),
-        sitemap(),
+        sitemap({
+            customPages: [
+                'https://stephenferguson.info/projects/next-js',
+                'https://stephenferguson.info/projects/php-laravel',
+                'https://stephenferguson.info/projects/wordpress',
+                'https://stephenferguson.info/projects/dev-ops',
+                'https://stephenferguson.info/projects/testing',
+                'https://stephenferguson.info/projects/packages',
+                'https://stephenferguson.info/projects/people-management',
+                'https://stephenferguson.info/projects/other-stuff'
+            ],
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date()
+        }),
     ],
-    site: "https://www.miFerguson.com",
+    site: "https://stephenferguson.info",
     scopedStyleStrategy: "where",
     image: {
         service: passthroughImageService()
